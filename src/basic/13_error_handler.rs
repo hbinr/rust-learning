7bdef3e0081f06f8da62ce60e98f3a07c5ca6057
hbinr <dhb1807227469@.163.com>
2022-01-27 18:16:58 +0800

    feat: error handler

--- src/basic/13_error_handler.rs
+++ src/basic/13_error_handler.rs
@@ -1,7 +1,23 @@
-fn error_handler() {
+fn error_handler_panic() {
     println!("error handler");
 
     // 数组越界panic示例
     let num_arr = vec![1, 2, 3];
     println!("{}", num_arr[4]);
+}
+
+fn error_handler_option() {
+    let fruits = vec!["banana", "apple", "coconut", "orange", "strawberry"];
+
+    // pick the first item:
+    let first = fruits.get(0);
+    println!("{:?}", first); // Some("banana")
+
+    // pick the third item:
+    let third = fruits.get(2);
+    println!("{:?}", third); // Some("coconut")
+
+    // pick the 99th item, which is non-existent:
+    let non_existent = fruits.get(99);
+    println!("{:?}", non_existent); // None
 }
